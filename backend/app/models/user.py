5cf382b5a4b9a4ee8148c36e470eac180a07663f
Haydan Patel <haydan.dev@gmail.com>
2026-03-08 14:17:51 +0530

    chore(setup): add initial User blueprint logic for future Pydantic refactor

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,19 +1,18 @@
+#imports 
 import uuid
 import datetime as dt
-from pydantic import BaseModel, Field
-user = {
-    "user_id" : str(uuid.uuid4()),
-    "full_name" : "",
-    "email" : "",
-    "password_hash" : "",
-    "profile_pic_url" : "",
-    "role" : "",
-    "is_online" : False,
-    "last_seen_at" : dt.datetime.now(),
-    "created_at" : dt.datetime.now(),
-    "contacts" : [
-    ],
-    "fcm_tokens" : {},
-    "is_deleted" : False,
-    "deleted_at" : None 
-}
+# classes and objects
+class User:
+   def __init__(self,full_name,email,password,profile_pic_url):
+      self.user_id = str(uuid.uuid4())
+      self.full_name = full_name
+      self.email = email
+      self.password = password
+      self.profile_pic_url = profile_pic_url
+      self.is_online = False
+      self.last_seen_at = dt.datetime.now()
+      self.created_at =  dt.datetime.now()
+      self.contacts = []
+      self.fcm_tokens = {}
+      self.is_deleted = False
+      self.deleted_at = None
